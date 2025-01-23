4667c1bb309433131e3e0338acebdc7611892546
JetlaggMC <118580793+JetlaggMC@users.noreply.github.com>
2025-01-23 18:59:22 -0500

    remove duplicate resource_combinator.js

--- kubejs/server_scripts/resource_combinator.js
+++
@@ -1,9 +0,0 @@
-ServerEvents.recipes(event => {
-	event.recipes.gtceu.resource_combinator('wheat_seeds_1')
-		.itemInputs(Item.of('64x minecraft:wheat_seeds'))
-		.inputFluids(Fluid.of('minecraft:water',1000))
-		.chancedOutput('1x kubejs:wheat_seeds_mk1', 500, 500)
-		.duration(1200)
-		.EUt(32)
-
-})
