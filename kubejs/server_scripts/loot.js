a573f3bb622c1c46ce3a640e53533fa2e54ce6fe
JetlaggMC <118580793+JetlaggMC@users.noreply.github.com>
2025-02-01 15:21:19 -0500

    replace twilight forest trophy drops

---
+++ kubejs/server_scripts/loot.js
@@ -0,0 +1,26 @@
+LootJS.modifiers((event) => {
+    event.addEntityLootModifier("twilightforest:naga")
+    .addLoot("kubejs:unborn_naga_trophy")
+    .removeLoot("twilightforest:naga_trophy")
+
+
+    event.addEntityLootModifier("twilightforest:lich")
+    .addLoot("kubejs:unborn_lich_trophy")
+    .removeLoot("twilightforest:lich_trophy")
+
+
+    event.addEntityLootModifier("twilightforest:hydra")
+    .addLoot("kubejs:unborn_hydra_trophy")
+    .removeLoot("twilightforest:hydra_trophy")
+
+
+    event.addEntityLootModifier("twilightforest:ur_ghast")
+    .addLoot("kubejs:unborn_urghast_trophy")
+    .removeLoot("twilightforest:ur_ghast_trophy")
+
+
+    event.addEntityLootModifier("twilightforest:snow_queen")
+    .addLoot("kubejs:unborn_snow_queen_trophy")
+    .removeLoot("twilightforest:snow_queen_trophy")
+
+    });
