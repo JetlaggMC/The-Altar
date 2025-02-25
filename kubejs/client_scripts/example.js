4dc2b4c76e1d1b972250a55a7f4735588f27716c
JetlaggMC <118580793+JetlaggMC@users.noreply.github.com>
2025-02-25 15:25:59 -0500

    hide some unused items and blocks

--- kubejs/client_scripts/example.js
+++ kubejs/client_scripts/example.js
@@ -1,6 +1,16 @@
 JEIEvents.hideItems(event => {
   event.hide('gtceu:andesite_alloy_ingot')
   event.hide('ad_astra:hammer')
+  event.hide('gtceu:ulv_dna_fabricator')
+  event.hide('mekanism:block_charcoal')
+  event.hide('mekanism:dust_charcoal')
+  event.hide('thermal:charcoal_block')
+  event.hide('thermal:machine_pyrolyzer')
+  event.hide('thermal:dynamo_stirling')
+  event.hide('gtceu:brittle_charcoal')
+  event.hide('gtceu:charcoal_pile_igniter')
+  event.hide('gtceu:firebrick')
+
 })
 
 ClientEvents.lang('en_us', event => {
