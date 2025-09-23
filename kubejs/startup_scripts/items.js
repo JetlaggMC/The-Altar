e93b54f9f0e0cee17840610ea791d83f600db2b3
JetlaggMC <118580793+JetlaggMC@users.noreply.github.com>
2025-09-23 00:05:54 -0400

    add 2nd fluid input to mega mixer

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -1037,7 +1037,7 @@ GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
         event.create('mega_mixer')
         .category('mega_mixer')
         .setEUIO('in')
-        .setMaxIOSize(8, 3, 1, 1) 
+        .setMaxIOSize(8, 3, 2, 1) 
         .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
         .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
         .setSound(GTSoundEntries.COOLING)
