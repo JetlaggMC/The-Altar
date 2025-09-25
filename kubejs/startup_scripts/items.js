4d6789e792c1242fdd004063a6507153c5c30df0
JetlaggMC <118580793+JetlaggMC@users.noreply.github.com>
2025-09-25 13:32:15 -0400

    breeding chamber recipetype

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -1074,4 +1074,12 @@ GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
         .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
         .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
         .setSound(GTSoundEntries.COOLING)
+
+        event.create('breeding_chamber')
+        .category('breeding_chamber')
+        .setEUIO('in')
+        .setMaxIOSize(4, 1, 1, 0) 
+        .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
+        .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
+        .setSound(GTSoundEntries.COOLING)
 })
