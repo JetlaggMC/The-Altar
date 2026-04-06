20fa17d0309eb8957588c8442a22c3b09dad8fbc
JetlaggMC <118580793+JetlaggMC@users.noreply.github.com>
2026-04-05 23:27:37 -0400

    Update items.js

--- kubejs/startup_scripts/items.js
+++ kubejs/startup_scripts/items.js
@@ -1,5 +1,5 @@
 const CoilWorkableElectricMultiblockMachine = Java.loadClass("com.gregtechceu.gtceu.api.machine.multiblock.CoilWorkableElectricMultiblockMachine")
-
+	
 StartupEvents.registry('item', e => {
   // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png
   // If you want a custom item model, you can create one in Blockbench and put it in kubejs/assets/kubejs/models/item/test_item.json
