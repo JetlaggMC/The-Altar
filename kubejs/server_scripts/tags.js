58a1f741e31d5d372f56d9978cb91081310b12a4
JetlaggMC <118580793+JetlaggMC@users.noreply.github.com>
2026-04-05 23:27:32 -0400

    add create wrench tag to mbd2 create blocks

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -1,8 +1,26 @@
 ServerEvents.tags('item', event => {
     event.add('forge:ingots/andesite_alloy', 'create:andesite_alloy')
+
     event.remove('forge:ores/niter', 'thermal:niter_ore')
     event.remove('forge:ores/niter', 'thermal:deepslate_niter_ore')
     event.remove('forge:dusts/niter', 'thermal:niter_dust')
     event.remove('forge:gems/niter', 'thermal:niter')
     event.remove('forge:storage_blocks/niter', 'thermal:niter_block')
+})
+
+
+ServerEvents.tags('block', event => {
+	event.add('create:wrench_pickup', 'mbd2:andesite_input_bus')
+	event.add('create:wrench_pickup', 'mbd2:andesite_output_bus')
+	event.add('create:wrench_pickup', 'mbd2:brass_input_bus')
+	event.add('create:wrench_pickup', 'mbd2:brass_output_bus')
+	event.add('create:wrench_pickup', 'mbd2:cupronickel_input_hatch')
+	event.add('create:wrench_pickup', 'mbd2:cupronickel_output_hatch')
+	event.add('create:wrench_pickup', 'mbd2:kinetic_stress_input')
+	event.add('create:wrench_pickup', 'mbd2:kinetic_foundry')
+	event.add('create:wrench_pickup', 'mbd2:casting_tower')
+	event.add('create:wrench_pickup', 'mbd2:large_mechanical_press')
+	event.add('create:wrench_pickup', 'mbd2:large_mechanical_mixer')
+	event.add('create:wrench_pickup', 'mbd2:andesite_mechanical_drill')
+	event.add('create:wrench_pickup', 'mbd2:brass_mechanical_drill')
 })
